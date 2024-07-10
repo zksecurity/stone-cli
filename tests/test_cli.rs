db30c4f2adaee997efd847a4dfcdd6fa774ce15b
mellowcroc <mellowcroc@gmail.com>
2024-07-10 23:23:20 +0900

    add e2e tests

--- tests/test_cli.rs
+++ tests/test_cli.rs
@@ -1,11 +1,15 @@
 use rstest::{fixture, rstest};
-use starknet_adapter::{build::setup as build_setup, cairo1::run_cairo1};
+use starknet_adapter::{
+    cairo1::run_cairo1, prover::run_stone_prover, starknet::run_starknet_verifier,
+    utils::set_env_vars,
+};
 use std::{env::current_exe, path::Path, path::PathBuf};
 use tempfile::TempDir;
 
 #[fixture]
 fn setup() {
-    build_setup();
+    const CONFIG: &[u8] = include_bytes!("../config.json");
+    set_env_vars(CONFIG);
 }
 
 #[rstest]
@@ -74,7 +78,6 @@ fn test_run_cairo1_success(
         Path::new(env!("CARGO_MANIFEST_DIR"))
     );
     let current_exe = current_exe().expect("Failed to get current executable");
-    println!("Current executable: {:?}", current_exe);
     let program_file = Path::new(env!("CARGO_MANIFEST_DIR"))
         .join("examples")
         .join(program);
@@ -93,6 +96,87 @@ fn test_run_cairo1_success(
     }
 
     check_tmp_files(&tmp_dir, &program_file);
+}
+
+#[rstest]
+#[case("recursive", "array_append.cairo")]
+#[case("recursive", "array_integer_tuple.cairo")]
+#[case("recursive", "bytes31_ret.cairo")]
+#[case("recursive", "enum_flow.cairo")]
+#[case("recursive", "enum_match.cairo")]
+#[case("recursive", "factorial.cairo")]
+#[case("recursive", "felt_span.cairo")]
+#[case("recursive", "fibonacci.cairo")]
+#[case("recursive", "hello.cairo")]
+#[case("recursive", "null_ret.cairo")]
+#[case("recursive", "pedersen_example.cairo")]
+#[case("recursive", "print.cairo")]
+#[case("recursive", "recursion.cairo")]
+#[case("recursive", "sample.cairo")]
+#[case("recursive", "simple_struct.cairo")]
+#[case("recursive", "simple.cairo")]
+#[case("recursive", "struct_span_return.cairo")]
+fn test_run_e2e(
+    #[from(setup)] _path: (),
+    #[case(layout)] layout: &str,
+    #[case(program)] program: &str,
+) {
+    let tmp_dir = tempfile::Builder::new()
+        .prefix("starknet-adapter-test-")
+        .tempdir()
+        .expect("Failed to create temp dir");
+    let current_exe = current_exe().expect("Failed to get current executable");
+    let program_file = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("examples")
+        .join(program);
+    let args = vec![
+        current_exe.to_str().unwrap(),
+        program_file.to_str().unwrap(),
+        "--layout",
+        layout,
+        "--proof_mode",
+    ]
+    .into_iter()
+    .map(|s| s.to_string());
+    match run_cairo1(args.clone(), &tmp_dir) {
+        Ok(result) => {
+            println!("Successfully ran cairo1: {:?}", result);
+
+            let filename = program_file.file_stem().unwrap().to_str().unwrap();
+            let air_public_input = tmp_dir
+                .path()
+                .join(format!("{}_air_public_input.json", filename));
+            let air_private_input = tmp_dir
+                .path()
+                .join(format!("{}_air_private_input.json", filename));
+            let dest_dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("tmp_files_backup");
+            if !dest_dir.exists() {
+                std::fs::create_dir_all(&dest_dir).expect("Failed to create destination directory");
+            }
+
+            for entry in std::fs::read_dir(&tmp_dir).expect("Failed to read tmp_dir") {
+                let entry = entry.expect("Failed to get entry");
+                let file_name = entry.file_name();
+                let dest_file = dest_dir.join(file_name);
+                std::fs::copy(entry.path(), dest_file).expect("Failed to copy file");
+            }
+            match run_stone_prover(&air_public_input, &air_private_input, &tmp_dir) {
+                Ok(result) => {
+                    println!("Successfully ran stone prover: {:?}", result.proof);
+                    let proof = result.proof;
+
+                    match run_starknet_verifier(args, &proof) {
+                        Ok(result) => {
+                            println!("Successfully ran starknet verifier: {:?}", result);
+                        }
+                        Err(e) => panic!("Expected a successful result but got an error: {:?}", e),
+                    }
+                }
+                Err(e) => panic!("Expected a successful result but got an error: {:?}", e),
+            }
+        }
+        Err(e) => panic!("Expected a successful result but got an error: {:?}", e),
+    }
 }
 
 fn assert_error_msg_eq(e: &anyhow::Error, expected: &str) {
